a12bd6ceed59ee5b18df31a75d777c5e37348e60
re4nightwing <dulan9595531@gmail.com>
2022-10-07 03:56:33 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        BGColor: '#191A1C',
+        BGColorLight: 'hsl(0, 0%, 17%)',
+        themeDark: '#48466D',
+        themeDarkLight: '#3D84A8',
+        themeMain: '#46CDCF',
+        themeLight: '#ABEDD8',
+      }
+    },
+  },
+  plugins: [],
+}
