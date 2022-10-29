e44b5232d35f7f3710752a05af357a4bbea8aca2
re4nightwing <dulan9595531@gmail.com>
2022-10-30 03:19:43 +0530

    chapter page UI done. Half way on db models for books and chapters

--- dist/temp_db_asset/models.py
+++ dist/temp_db_asset/models.py
@@ -1,14 +1,22 @@
-#from webapp.extensions import db
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from fastapi_utils.guid_type import GUID
-from uuid import uuid4
-from webapp import db
+
+db_user = 'dulan'
+db_pswd = 'good'
+db_host = 'localhost'
+db_name = 'llamaloops_db'
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_pswd}@{db_host}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class Book(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100), nullable=False, unique=True)
   alt_title = db.Column(db.String(200), nullable=True, unique=False)
-  cover_img = db.Column(db.String(100), nullable=False, unique=False, default="default_cover.jpg")
+  cover_img = db.Column(db.String(100), nullable=False, unique=False, server_default="default_cover.jpg")
   synopsis = db.Column(db.String(1000), nullable=True, unique=False)
   type = db.Column(db.String(50), nullable=False, unique=False)
   status = db.Column(db.String(50), nullable=False, unique=False)
@@ -17,6 +25,7 @@ class Book(db.Model):
   created = db.Column(db.DateTime, server_default=db.func.now())
   updated = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
   author_id = db.Column(db.Integer, db.ForeignKey('author.id'),nullable=False)
+  chapters = db.relationship('Chapter', backref="book")
 
   def __repr__(self):
     return f'{self.title}'
@@ -25,7 +34,7 @@ class Chapter(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   order = db.Column(db.Integer, nullable=False, unique=True)
   display_number = db.Column(db.String(10), nullable=False, unique=False)
-  audio_url = db.Column(db.String(100), nullable=False, unique=False, default="sample.mp3")
+  audio_url = db.Column(db.String(100), nullable=False, unique=False, server_default="sample.mp3")
   read_text_url = db.Column(db.String(100), nullable=True, unique=False)
   views = db.Column(db.Integer, nullable=False, default=0)
   created = db.Column(db.DateTime, server_default=db.func.now())
@@ -49,6 +58,7 @@ class Author(db.Model):
   name = db.Column(db.String(200), nullable=False, unique=True)
   created = db.Column(db.DateTime, server_default=db.func.now())
   updated = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+  books = db.relationship('Book', backref="author")
 
   def __repr__(self):
     return f'{self.name}'
